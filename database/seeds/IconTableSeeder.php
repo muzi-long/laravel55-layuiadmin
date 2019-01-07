26627761d968b05faba4e941aeb01e785ec5ca4c
github-muzilong <712120393@qq.com>
2019-01-07 09:31:33 +0800

    更新路径

--- database/seeds/IconTableSeeder.php
+++ database/seeds/IconTableSeeder.php
@@ -12,7 +12,7 @@ class IconTableSeeder extends Seeder
     public function run()
     {
         \App\Models\Icon::truncate();
-        $file = file_get_contents(storage_path('app').'\public\icons.json');
+        $file = file_get_contents(storage_path('app').'/public/icons.json');
         $icons = json_decode($file,true);
         foreach ($icons as $icon){
             \App\Models\Icon::create($icon);
